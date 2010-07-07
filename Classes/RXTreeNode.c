459d7b7bbc849989a5c2cb4384bb4399c2362abd
Rob RIx <rob@monochromeindustries.com>
2010-07-07 06:00:09 -0400

    Initial commit.

---
+++ Classes/RXTreeNode.c
@@ -0,0 +1,7 @@
+// RXTreeNode.m
+// Created by Rob Rix on 2010-07-07
+// Copyright 2010 Monochrome Industries
+
+#include "RXTreeNode.h"
+
+
---
+++ Classes/RXTreeNode.h
@@ -0,0 +1,10 @@
+// RXTreeNode.h
+// Created by Rob Rix on 2010-07-07
+// Copyright 2010 Monochrome Industries
+
+#ifndef RX_TREE_NODE
+#define RX_TREE_NODE
+
+
+
+#endif
